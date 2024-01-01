e1c9c8e95cfc0999b981de463a974953afe5ce4a
Iqbal Abdul Ra'uf <iqbalar@student.telkomuniversity.ac.id>
2024-01-01 16:53:21 +0700

    change some information

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,38 +4,38 @@
 const projects = [
 	{
 		id: 1,
-		title: 'Google Health Platform',
+		title: 'LaRoyba Classic Furniture',
 		category: 'Web Application',
 		img: require('@/assets/images/web-project-2.jpg'),
 	},
 	{
 		id: 2,
-		title: 'Phoenix Digital Agency',
+		title: 'E-Government Report App',
 		category: 'Mobile Application',
 		img: require('@/assets/images/mobile-project-2.jpg'),
 	},
 	{
 		id: 3,
-		title: 'Project Management UI',
+		title: 'Social-Media Based Covid Tracker',
 		category: 'UI/UX Design',
 		img: require('@/assets/images/ui-project-1.jpg'),
 	},
 	{
 		id: 4,
-		title: 'Cloud Storage Platform',
-		category: 'UI/UX Design',
+		title: 'SuperIdol Integrated Apps',
+		category: 'Mobile Application',
 		img: require('@/assets/images/ui-project-2.jpg'),
 	},
 	{
 		id: 5,
-		title: 'React Social App',
-		category: 'Mobile Application',
+		title: 'Aksarapers.com News',
+		category: 'Content Writing',
 		img: require('@/assets/images/mobile-project-1.jpg'),
 	},
 	{
 		id: 6,
-		title: 'Apple Design System',
-		category: 'Web Application',
+		title: 'ArgumentasiRealiti Project',
+		category: 'Content Writing',
 		img: require('@/assets/images/web-project-1.jpg'),
 	},
 ];
