658c2965d868a73fd1c97374fb2195e56f18a189
Iqbal Abdul Ra'uf <iqbalar@student.telkomuniversity.ac.id>
2024-02-19 23:01:46 +0700

    Update info

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -6,17 +6,17 @@ export default {
 				{
 					id: 1,
 					bio:
-						'Saya adalah seorang mahasiswa Software Engineering. Saat ini saya sedang mendalami mengenai mobile programming, khususnya di Flutter. Selain itu, saya tertarik mengenai Web Development, khususnya mengenai Frontend Developer dan PHP Laravel. Saya juga pernah mengerjakan penelitian untuk tesis dan proyek mengenai desain UI/UX.',
+						'Saya merupakan lulusan S1 Rekayasa Perangkat Lunak Telkom University yang tertarik mendalami mobile development, software quality assurance, dan UI/UX Design. Saya juga pernah mengerjakan penelitian untuk tugas akhir dan proyek mengenai desain UI/UX.',
 				},
 				{
 					id: 2,
 					bio:
-						'Saya pernah terlibat beberapa project untuk pembuatan website untuk perusahaan, meliputi pengerjaan desain, pembangunan website, hingga penyusunan konten dan penyuntingan foto produk.',
+						'Saya juga terlibat beberapa project untuk pembuatan website company profile untuk perusahaan, meliputi pengerjaan desain, pembangunan website, hingga penyusunan konten dan penyuntingan foto produk.',
 				},
 				{
 					id: 3,
 					bio:
-						'Selama menjadi mahasiswa, saya pernah menjadi asisten dosen dan asisten di laboratorium untuk Object-Oriented Programming dan SE Implementation and Testing. Beberapa project di portfolio ini merupakan pengembangan dari tugas perkuliahan yang pernah saya kerjakan. Saya juga pernah menulis untuk pers mahasiswa, saya telah menulis 15 artikel selama 2 tahun sebagai staf dan pimpinan redaksi. Setelah ini, saya akan melanjutkan untuk mengembangkan konten kreator untuk penggunaan teknologi harian untuk masyarakat.',
+						'Selama menjadi mahasiswa, saya pernah menjadi asisten dosen dan asisten di laboratorium untuk Object-Oriented Programming dan SE Implementation and Testing. Beberapa project di portfolio ini merupakan pengembangan dari tugas perkuliahan yang pernah saya kerjakan. Saya juga pernah menulis untuk pers mahasiswa, saya telah menulis 15 artikel selama 2 tahun sebagai staf dan pimpinan redaksi.',
 				},
 			],
 			socialSharingsHeading: 'Media Sosial saya',
