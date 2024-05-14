7ef6dc483c4f4ab039261c495f3a14931c2e95f0
iqbalarauf <iqbalarauf.official@gmail.com>
2024-05-14 10:10:57 +0700

    add maze controller code

--- src/App.vue
+++ src/App.vue
@@ -99,3 +99,25 @@ export default {
 	}
 }
 </style>
+
+<script>
+(function (m, a, z, e) {
+  var s, t;
+  try {
+    t = m.sessionStorage.getItem('maze-us');
+  } catch (err) {}
+
+  if (!t) {
+    t = new Date().getTime();
+    try {
+      m.sessionStorage.setItem('maze-us', t);
+    } catch (err) {}
+  }
+
+  s = a.createElement('script');
+  s.src = z + '?apiKey=' + e;
+  s.async = true;
+  a.getElementsByTagName('head')[0].appendChild(s);
+  m.mazeUniversalSnippetApiKey = e;
+})(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '9cf17bb6-c22d-4bac-9a9f-c64773047481');
+</script>
